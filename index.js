119e4b5a7d638010458993069a16922eb6aa3fd4
Kay B <kaylouisebennett@gmail.com>
2018-07-24 16:20:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
-Object.assign({}, { prop: 1 }, {prop2: 2})
+Object.assign({ prop: 1 }, {prop2: 2})
 return recipes
 }
