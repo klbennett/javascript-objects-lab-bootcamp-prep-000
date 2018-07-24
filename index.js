270c6151be7ca96257918b39fb5fb854da2e9ba2
Kay B <kaylouisebennett@gmail.com>
2018-07-24 16:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
  
  function deleteFromObjectByKey(object, key) {
-  Object.assign({}, recipes,)
+var myNewObj = Object.assign({}, recipes)
+mynewObj
  }
